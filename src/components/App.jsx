3252c0f690c5469bf90e676d446cad8569b066a2
JurekZela <122160360+JurekZela@users.noreply.github.com>
2024-01-01 13:53:54 +0100

    add useState

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,49 +1,51 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { FeedbackCard } from './Feedback/Feedback-styled';
 import { Statistics } from './Statistics/Statistics';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Section } from './Section/Section';
 
-export class App extends Component{
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0
-  }
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  onLeaveFeedback = event => {
-    this.setState({ [event]: this.state[event] + 1 });
+  const objTotal = { good, neutral, bad };
+
+  const onLeaveFeedback = e => {
+    if (e === 'good') {
+      return setGood(prevGood => prevGood + 1);
+    }
+
+    if (e === 'neutral') { 
+      return setNeutral(prevNeutral => prevNeutral + 1);
+    }
+
+    if (e === 'bad') {
+      return setBad(prevBad => prevBad + 1);
+    }
   };
 
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state
-    return good + neutral + bad;
-  };
-
-  countPositiveFeedbackPercentage = () => {
-    const total = this.countTotalFeedback();
+  const countTotalFeedback = () => good + neutral + bad;
+  
+  const countPositiveFeedbackPercentage = () => {
+    const total = countTotalFeedback();
     if (!total) {
       return 0;
     }
-    const percent = (this.state.good * 100) / total;
+    const percent = (good * 100) / total;
     return Math.round(percent);
   };
-
-  render() {
-    const { good, neutral, bad } = this.state
-    const total = good + neutral + bad;
 
     return (
         <FeedbackCard>
          <Section title="Please leave feedback">
          <FeedbackOptions
-         options={Object.keys(this.state)}
-         onLeaveFeedback={this.onLeaveFeedback}></FeedbackOptions>
+         options={Object.keys(objTotal)}
+         onLeaveFeedback={onLeaveFeedback} />
          </Section>
          <Section title="Statistics">
-           <Statistics good={good} neutral={neutral} bad={bad} total={total} positivePercentage={this.countPositiveFeedbackPercentage()}></Statistics>  
+           <Statistics good={good} neutral={neutral} bad={bad} total={countTotalFeedback()} positivePercentage={countPositiveFeedbackPercentage()}></Statistics>  
         </Section>
         </FeedbackCard>
     );
-  }
-};
+  };
